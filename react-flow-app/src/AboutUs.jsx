68973df500878aefbabaab083c1714c269f65eca
DHRUTHI YADAV <dhruthi1988@gmail.com>
2024-11-02 12:01:21 +0530

    ratan sir modification

--- react-flow-app/src/AboutUs.jsx
+++ react-flow-app/src/AboutUs.jsx
@@ -2,9 +2,11 @@ function AboutUs()
 {
    return(
     <>
+    <h1>This is react flow app</h1>
     <h1>About Us page</h1>
     <p>This is a aboutUs.jsx file</p>
     </>
    )
 }
 export default AboutUs;
+
